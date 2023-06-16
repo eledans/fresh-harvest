a4b734003c1e79d370e2775d093aa3adf9dc8e01
Elia Dania Serepina <80114768+eledans@users.noreply.github.com>
2023-06-16 22:27:04 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from tensorflow import keras
+from flask import Flask, render_template, request, jsonify
+from predict import fruit
 
 # flask utils
-from flask import Flask, render_template, redirect, url_for, request
+from flask_cors import CORS, cross_origin
+from utils.utils import decodeImage
+
+import os
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 # define a flask app
 app = Flask(__name__)
 
-# Load the model from the .h5 file
-model = keras.models.load_model('model\model.h5')
+
+class ClientApp:
+    def __init__(self):
+        self.filename = "logo.png"
+        self.classifier = fruit(self.filename)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -15,10 +25,11 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    feature = [np.array(float_features)]
-    prediction = model.predict(feature)
-    return render_template("index.html", prediction_text = "{}").format(prediction)
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.fruitCheck()
+    return jsonify(result)
 
 if __name__== '__main__':
+    clApp = ClientApp()
     app.run(port=3000, debug=True)
